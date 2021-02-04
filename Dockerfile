42a8b0c43c61ebe54b3908ed280e8c88b04d13ac
lee <shida23577@163.com>
2021-02-04 16:26:16 +0800

    1.添加qt库

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM centos:centos7.8.2003
 MAINTAINER "joinbright"
 ADD ext_lib /root/ext_lib/
 ADD https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.gz /root/ext_lib/
+ADD https://download.qt.io/archive/qt/4.8/4.8.4/qt-everywhere-opensource-src-4.8.4.tar.gz /root/ext_lib/
 RUN yum -y install gcc gcc-c++ make passwd openssl openssh-server lsof openssh-clients svn rpm-build java-1.8.0-openjdk-headless java-1.8.0-openjdk && yum clean all \
  && sed -i 's|session    required     pam_loginuid.so|session    optional     pam_loginuid.so|g' /etc/pam.d/sshd \
  && mkdir -p /var/run/sshd/ \
--- ext_lib/build.sh
+++ ext_lib/build.sh
@@ -15,6 +15,11 @@ tar -zxvf boost_1_73_0.tar.gz
 cd boost_1_73_0
 ./bootstrap.sh
 ./b2 install --prefix=/usr/local cxxflags='-fPIC' cflags='-fPIC' toolset=gcc link=static runtime-link=shared threading=multi --with-filesystem --with-iostreams --with-regex --with-serialization --with-system --with-date_time --with-locale address-model=64
+cd ..
+echo -e "build qt ---------"
+tar -zxvf qt-everywhere-opensource-src-4.8.4.tar.gz
+cd qt-everywhere-opensource-src-4.8.4
+./configure && make install echo "export QTDIR=/usr/local/Trolltech/Qt-4.8.4\r\n export PATH=$QTDIR/bin:$PATH\r\n export MANPATH=$QTDIR/man:$MANPAT \r\n export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH" >> .bash_profile && source .bash_profile
 echo -e "build fininshed!!!!"
 
 rm -rf /root/ext_lib/*
